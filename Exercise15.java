2e8ded152e3ce8ab131121d60f667375a33b8059
Schkalman <47022709+Schkalman@users.noreply.github.com>
2020-10-17 11:18:34 +0200

    Add files via upload

---
+++ Exercise15.java
@@ -0,0 +1,16 @@
+package exercise;
+
+public class Exercise15 {
+	 
+	 public static void main(String[] args) {
+	     
+	   int a, b, temp;
+	   a = 15;
+	   b = 27;
+	   System.out.println("Före byte : a, b = "+a+", "+ + b);
+	   temp = a;
+	   a = b;
+	   b = temp;   
+	  System.out.println("Efter byte : a, b = "+a+", "+ + b);
+	 }
+	 }
